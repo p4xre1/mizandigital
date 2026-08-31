9372a2f83acb03afd77ed1bb5dd1af9602c4d239
Yassin Mohamed Reda <mohamedreda.yassin@etu.uae.ac.ma>
2026-08-31 21:25:07 +0100

    fix

--- src/lib/r2/upload.ts
+++ src/lib/r2/upload.ts
@@ -1,6 +1,4 @@
-import { S3Client } from "@aws-sdk/client-s3";
-import { PutObjectCommand } from "@aws-sdk/client-s3/commands/PutObjectCommand";
-import { DeleteObjectCommand } from "@aws-sdk/client-s3/commands/DeleteObjectCommand";
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const r2Client = new S3Client({
